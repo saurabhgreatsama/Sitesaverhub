3b96ec37b9ab0304b4f2da03277511d7dc34fc9d
GREAT SAMA <109392018+saurabhgreatsama@users.noreply.github.com>
2024-12-18 21:53:07 +0530

    Update header.php

--- application/views/header.php
+++ application/views/header.php
@@ -36,15 +36,9 @@
 
     <link rel="stylesheet" href="<?php echo base_url("news/css/flaticon.css") ?>">
     <link rel="stylesheet" href="<?php echo base_url("news/css/style.css") ?>">
-    <!--Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5WG8BJRGSP"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+   
 
-  gtag('config', 'G-5WG8BJRGSP');
-</script>
+
 </head>
 
 <body>
